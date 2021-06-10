54eeab2445a3d4a3bcbd6b76d243f57176556b2b
kutyrov <kutyrov.andrey@gmail.com>
2021-06-10 22:47:57 +0300

    got idea of ascincio

--- server.py
+++ server.py
@@ -1,19 +1,25 @@
 import asyncio
 
 
+def process_data(data):
+    return "123"
+
+
 class ClientServerProtocol(asyncio.Protocol):
     def connection_made(self, transport):
+        # print("Connection made ", transport) # это неважно
         self.transport = transport
 
     def data_received(self, data):
-        resp = process_data(data.decode())
-        self.transport.write(resp.encode())
+        print("data received ", data)  # данные пришли
+        resp = process_data(data.decode())  # обработали сформировари ответ
+        self.transport.write(resp.encode())  # отправили
 
 
 loop = asyncio.get_event_loop()
 coro = loop.create_server(
     ClientServerProtocol,
-    '127.0.0.1', 8181
+    '127.0.0.1', 8888
 )
 
 server = loop.run_until_complete(coro)
